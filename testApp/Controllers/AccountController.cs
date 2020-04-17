fb467a9bc2e0170d0f2c0bd585e4ca4e4ca2261f
prairit <54899228+prairit@users.noreply.github.com>
2020-04-17 21:40:28 +0530

    added projet files with formauthentication implemented

---
+++ testApp/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using testApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using testApp.ViewModel;
+
+namespace MvcFormAuthentication_Demo.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        private readonly authEntities _dbContext = new authEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                bool IsValidUser = _dbContext.Users
+               .Any(u => u.Username.ToLower() == user
+               .UserName.ToLower() && user
+               .UserPassword == user.UserPassword);
+
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserName, false);
+                    return RedirectToAction("Index", "Employee");
+                }
+            }
+            ModelState.AddModelError("", "invalid Username or Password");
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User registerUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Users.Add(registerUser);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Login");
+
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
